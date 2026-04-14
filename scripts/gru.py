48df3edb30facad21a639644bff78ec03e498fc2
Emwook <lukasiuk.emilia@gmail.com>
2026-04-14 13:16:02 +0200

    implementing a gru cell

--- scripts/gru.py
+++ scripts/gru.py
@@ -1,5 +1,57 @@
 from get_data import *
 import torch as tr
 
-print(f"tr version: {tr.__version__}")
-print(f"tr backend available: {tr.backends.mps.is_available()}")
+# goal: implement gru cell
+# why: to use in gru network
+# how: use the definition found online 
+
+# the equations:
+#
+# GRU CELL
+# s = sigma function
+# z(t)  = s(W_z*x(t) + U_z*h(t-1) + b_z)
+# r(t)  = s(W_r*x(t) + U_r*h(t-1) + b_r)
+# h'(t) = tanh(U*r(t)*h(t-1) + W*x(t) + b_h')
+# h(t)  = (1-z(t))*h'(t) + h(t-1)*z(t)
+#
+# LOSS FUNCTION
+# Loss = MSE(theta_pred, theta_actual) + lambda*L
+# L = 1/3*l*theta_dd_2 + 1/2*l*theta_dd_1*cos(theta_1-theta_2) 
+#    - 1/2*l*theta^2_d_1*sin(theta_1 - theta_2) + 1/2g*sin(theta_2)
+
+#note:
+# @ - matmul (matrix)
+# * - mul (element wise)
+
+class Gru_Cell(tr.nn.Module):
+    def __init__(self, input_size, hidden_size):
+        super().__init__()
+        self.W_r = tr.nn.Parameter(tr.randn(input_size,hidden_size))
+        self.U_r = tr.nn.Parameter(tr.randn(hidden_size,hidden_size))
+        self.b_r = tr.nn.Parameter(tr.zeros(hidden_size))
+
+        self.W_z = tr.nn.Parameter(tr.randn(input_size,hidden_size))
+        self.U_z = tr.nn.Parameter(tr.randn(hidden_size,hidden_size))
+        self.b_z = tr.nn.Parameter(tr.zeros(hidden_size))
+
+        self.W = tr.nn.Parameter(tr.randn(input_size,hidden_size))
+        self.U = tr.nn.Parameter(tr.randn(hidden_size,hidden_size))
+        self.b = tr.nn.Parameter(tr.zeros(hidden_size))
+    
+    def get_reset_gate(self, x, h_prev):
+        return tr.sigmoid((x @ self.W_r) + (h_prev @ self.U_r) + self.b_r)
+
+    def get_update_gate(self, x, h_prev):
+        return tr.sigmoid((x @ self.W_z) + (h_prev @ self.U_z) + self.b_z)
+    
+    def get_candidate_gate(self, x, h_prev):
+        r = self.get_reset_gate(x, h_prev)
+        return tr.tanh(((r * h_prev) @ self.U) + (x @ self.W) + self.b)
+    
+    def get_new_h(self, x, h_prev):
+        z = self.get_update_gate(x, h_prev)
+        h_c = self.get_candidate_gate(x, h_prev)
+        return (((1-z) * h_c) + (h_prev) * z)
+    
+
+
